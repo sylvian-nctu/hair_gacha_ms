f5eb6e51bc9dcbde1bb87edbfb02a3d56314dd1a
sylvian-nctu <lugu7000013@gmail.com>
2025-01-21 20:21:20 +0800

    PRS NA

---
+++ data/20250122.js
@@ -0,0 +1,15 @@
+const version = '20250122';
+const hairstyles = [
+    { name: '小不點髮型', probability: 0.75, image: `image/${version}/01.png` },
+    { name: '彗星髮型', probability: 0.75, image: `image/${version}/02.png` },
+    { name: '迷霧賽琳髮型', probability: 0.75, image: `image/${version}/03.png` },
+    { name: '拉里艾髮型', probability: 0.75, image: `image/${version}/04.png` },
+    { name: '可愛風格頭', probability: 7.50, image: `image/${version}/05.png` },
+    { name: '短劉海髮', probability: 7.50, image: `image/${version}/06.png` },
+    { name: '露查莉亞頭', probability: 7.50, image: `image/${version}/07.png` },
+    { name: '親和雙辮造型', probability: 7.50, image: `image/${version}/08.png` },
+    { name: '長髮娃娃造型', probability: 16.75, image: `image/${version}/09.png` },
+    { name: '自然短髮造型', probability: 16.75, image: `image/${version}/10.png` },
+    { name: '逆風飄揚造型', probability: 16.75, image: `image/${version}/11.png` },
+    { name: '波希米亞造型', probability: 16.75, image: `image/${version}/12.png` }
+];
